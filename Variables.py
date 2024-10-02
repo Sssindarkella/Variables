8dec6f6286fa7ead9e360aacd55ca715b6f5e3b9
Sindarkella <sssindarkella@gmail.com>
2024-10-02 23:08:45 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,5 @@
+Number_of_homeworks = 12
+Hours = 1.5
+Course = 'Python'
+Time_of_1_task = Hours/Number_of_homeworks
+print('Курс: ',Course,', всего задач: ',Number_of_homeworks,', затрачено часов: ',Hours,', среднее время выполнения ',Time_of_1_task,'.')
